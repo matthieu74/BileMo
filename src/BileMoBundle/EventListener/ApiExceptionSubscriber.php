add3a097d8e210caceb88ec60ea6eea3e5eac76c
matthieu74 <peromatthieu@gmail.com>
2017-07-01 19:29:05 +0200

    a tested version

--- src/BileMoBundle/EventListener/ApiExceptionSubscriber.php
+++ src/BileMoBundle/EventListener/ApiExceptionSubscriber.php
@@ -19,16 +19,27 @@ class ApiExceptionSubscriber implements EventSubscriberInterface
 		if ($e instanceof ApiProblemException) {
 			$apiProblem = $e->getApiProblem();
 		} else {
-			$statusCode = $e instanceof HttpExceptionInterface ? $e->getStatusCode() : 500;
-			
+		    if ($e instanceof HttpExceptionInterface)
+			    $statusCode =  $e->getStatusCode();
+		    else if ($e instanceof BadCredentialsException)
+                return $e;
+		    else if ($e instanceof CustomUserMessageAuthenticationException)
+		        return $e;
+		    else
+                $statusCode = 500;
+
 			$apiProblem = new ApiProblem(
 					$statusCode
 					);
-			
+
 
 			if ($e instanceof HttpExceptionInterface) {
-				$apiProblem->set('detail', $e->getMessage());
-			}
+                $apiProblem->set('detail', $e->getMessage());
+            }
+            else if ($e instanceof \Exception) {
+                $apiProblem->set('detail', $e->getMessage());
+            }
+
 		}
 		
 		$response = new JsonResponse(
